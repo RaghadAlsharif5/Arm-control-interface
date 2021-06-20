c73252e24503cff58bfcb08d71679cd6c7689ddd
RaghadAlsharif5 <85364184+RaghadAlsharif5@users.noreply.github.com>
2021-06-20 23:50:19 +0300

    Add files via upload

---
+++ js file.js
@@ -0,0 +1,48 @@
+var Motor1 = document.getElementById("Motor1");
+var output1 = document.getElementById("value1");
+output1.innerHTML = Motor1.value;
+
+Motor1.oninput = function () {
+    output1.innerHTML = this.value;
+}
+
+var Motor2 = document.getElementById("Motor2");
+var output2 = document.getElementById("value2");
+output2.innerHTML = Motor2.value;
+
+Motor2.oninput = function () {
+    output2.innerHTML = this.value;
+}
+
+var Motor3 = document.getElementById("Motor3");
+var output3 = document.getElementById("value3");
+output3.innerHTML = Motor3.value;
+
+Motor3.oninput = function () {
+    output3.innerHTML = this.value;
+}
+
+var Motor4 = document.getElementById("Motor4");
+var output4 = document.getElementById("value4");
+output4.innerHTML = Motor4.value;
+
+Motor4.oninput = function () {
+    output4.innerHTML = this.value;
+}
+
+
+var Motor5 = document.getElementById("Motor5");
+var output5 = document.getElementById("value5");
+output5.innerHTML = Motor5.value;
+
+Motor5.oninput = function () {
+    output5.innerHTML = this.value;
+}
+
+var Motor6 = document.getElementById("Motor6");
+var output6 = document.getElementById("value6");
+output6.innerHTML = Motor6.value;
+
+Motor6.oninput = function () {
+    output6.innerHTML = this.value;
+}
